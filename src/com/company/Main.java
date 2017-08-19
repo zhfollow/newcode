ade347d65822ade3d233674cac1aef34750e25e1
zhfollow <30629543+zhfollow@users.noreply.github.com>
2017-08-19 12:55:39 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+/*
+
+
+块注释
+ */
+public class Main {
+
+
+    public static void print(int index,Object obj){
+        System.out.print(String.format("{%d},%s",index,obj));
+    }
+    
+
+
+    public static void main(String[] args) {
+	// write your code here
+        print(1,"aaaaa");
+    }
+}
